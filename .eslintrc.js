de26c762088006c75f477c0ee54c1b29236b6731
Kamigami55 <eason@easonchang.com>
2022-03-13 15:29:13 +0800

    Rewrite with my own config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,104 +1,39 @@
 module.exports = {
   extends: [
-    'airbnb',
-    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
+  plugins: ['html', 'prettier', 'react-hooks', 'import', 'simple-import-sort'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
     babelOptions: {
       presets: ['@babel/preset-react'],
     },
+    ecmaVersion: 'latest', // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
   },
   env: {
-    browser: true,
-    node: true,
+    browser: true, // Enables browser globals like window and document
+    node: true, // Enables Node.js global variables and Node.js scoping.
     jquery: true,
     jest: true,
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    es6: true,
   },
   rules: {
-    'no-debugger': 0,
-    'no-alert': 0,
-    'no-await-in-loop': 0,
-    'no-return-assign': ['error', 'except-parens'],
-    'no-restricted-syntax': [
-      2,
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-    'no-unused-vars': [
-      1,
-      {
-        ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err',
-      },
-    ],
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'all',
-      },
-    ],
-    'arrow-body-style': [2, 'as-needed'],
-    'no-unused-expressions': [
-      2,
-      {
-        allowTaggedTemplates: true,
-      },
-    ],
-    'no-param-reassign': [
-      2,
-      {
-        props: false,
-      },
-    ],
-    'no-console': 0,
-    'func-names': 0,
-    'space-before-function-paren': 0,
-    'comma-dangle': 0,
-    'max-len': 0,
-    'no-underscore-dangle': 0,
-    'consistent-return': 0,
-    'react/display-name': 1,
-    'react/no-array-index-key': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/prefer-stateless-function': 0,
-    'react/forbid-prop-types': 0,
-    'react/no-unescaped-entities': 0,
-    'react/function-component-definition': 0,
-    'jsx-a11y/accessible-emoji': 0,
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        assert: 'either',
-      },
-    ],
-    'react/require-default-props': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
-      },
-    ],
-    radix: 0,
-    'no-shadow': [
-      2,
-      {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
-      },
-    ],
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true,
-      },
-    ],
     'prettier/prettier': [
       'error',
       {
@@ -106,24 +41,11 @@ module.exports = {
         singleQuote: true,
         printWidth: 80,
         semi: false,
-        // below line only for windows users facing CLRF and eslint/prettier error
-        // non windows users feel free to delete it
-        endOfLine: 'auto',
       },
     ],
-    'jsx-a11y/href-no-hash': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'warn',
-      {
-        aspects: ['invalidHref'],
-      },
-    ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/comma-dangle': ['off'],
-    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
   },
-  plugins: ['html', 'prettier', 'react-hooks', 'import', 'simple-import-sort'],
 }
